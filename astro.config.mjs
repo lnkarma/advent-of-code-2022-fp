e92ab65379ca483741a334e85cb97ae2791efbde
Lokenath Karmakar <ilokenathkarmakar@gmail.com>
2023-03-11 07:19:54 +0000

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://lnkarma.github.io',
+  base: '/advent-of-code-2022-fp',
+});
