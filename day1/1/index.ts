5e44fffb40673bb3c07a02f46cf18f1445873232
Lokenath Karmakar <ilokenathkarmakar@gmail.com>
2023-03-11 16:37:22 +0000

    day 1.1 wip

--- day1/1/index.ts
+++ day1/1/index.ts
@@ -1,1 +1,15 @@
-console.log("asfdasfd");
+import * as A from "fp-ts/lib/Array";
+import { toError, fold } from "fp-ts/lib/Either";
+import { flow, pipe } from "fp-ts/lib/function";
+import { tryCatch, map, match, matchEW } from "fp-ts/lib/TaskEither";
+import { readFile } from "fs/promises";
+import path, { dirname } from "path";
+import { split, sum,  } from "ramda";
+
+export const getFileContents = () =>
+  tryCatch(() => readFile(path.join(__dirname, "input.txt"), "utf-8"), toError);
+
+const a = pipe(
+  getFileContents(),
+  map(flow(split("\n\n"), A.map(flow(split("\n"), A.map(parseInt), sum))))
+)().then(flow(fold(console.log, console.log)));
