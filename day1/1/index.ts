23869b69cea40fcdaa5cd56c91fc8ea69c40187f
lnkarma <ilokenathkarmakar@gmail.com>
2023-03-11 23:13:49 +0530

    solution to day1.1

--- day1/1/index.ts
+++ day1/1/index.ts
@@ -1,15 +1,36 @@
 import * as A from "fp-ts/lib/Array";
 import { toError, fold } from "fp-ts/lib/Either";
-import { flow, pipe } from "fp-ts/lib/function";
-import { tryCatch, map, match, matchEW } from "fp-ts/lib/TaskEither";
+import { flow, identity, pipe } from "fp-ts/lib/function";
+import { of, map as mapTask } from "fp-ts/lib/Task";
+import {
+  tryCatch,
+  map,
+  match,
+  matchEW,
+  mapLeft,
+  getOrElse,
+  getOrElseW,
+  bimap,
+  orElseFirst,
+  orElse,
+  orElseW,
+} from "fp-ts/lib/TaskEither";
 import { readFile } from "fs/promises";
 import path, { dirname } from "path";
-import { split, sum,  } from "ramda";
+import { apply, max, reject, split, sum } from "ramda";
 
 export const getFileContents = () =>
   tryCatch(() => readFile(path.join(__dirname, "input.txt"), "utf-8"), toError);
 
-const a = pipe(
+pipe(
   getFileContents(),
-  map(flow(split("\n\n"), A.map(flow(split("\n"), A.map(parseInt), sum))))
-)().then(flow(fold(console.log, console.log)));
+  map(
+    flow(
+      split("\n\n"),
+      A.map(flow(split("\n"), A.map(parseInt), reject(isNaN), sum)),
+      apply(Math.max)
+    )
+  ),
+  getOrElseW(of),
+  mapTask(console.log)
+)();
