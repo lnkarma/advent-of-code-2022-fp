1f0c95982fdfe4099ded452d6ea1a6f062c00f10
Lokenath Karmakar <ilokenathkarmakar@gmail.com>
2023-03-11 07:37:29 +0000

    try changing base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lnkarma.github.io',
-  base: '/advent-of-code-2022-fp',
+  base: '/advent-of-code-2022-fp/test',
 });
